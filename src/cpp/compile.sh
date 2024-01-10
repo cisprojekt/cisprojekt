b6f1014bb8a4e697b5720ccb46c3a4f7ca4db633
Jonas <kramerqk@proton.me>
2024-01-10 01:39:49 +0100

    changed compilation instructions and compiled to wasm

--- src/cpp/compile.sh
+++ src/cpp/compile.sh
@@ -2,7 +2,7 @@
 
 # Set default EIGEN_PATH and PROJECT_ROOT variables
 EIGEN_PATH=${EIGEN_PATH:-/usr/include/eigen3/}
-PROJECT_ROOT=${PROJECT_ROOT:-/mnt/c/Program\ Files/Ampps/www/main/}
+PROJECT_ROOT=${PROJECT_ROOT:-/mnt/c/Program\ Files/Ampps/www/compile_test/}
 
 # Printing
 GREEN='\033[0;32m'
@@ -14,7 +14,8 @@ echo -e "eigen_path: ${ORANGE}$EIGEN_PATH${NC}"
 echo -e "project_root: ${ORANGE}$PROJECT_ROOT${NC}\n"
 
 # Use variables to compile
-em++ -o ../../build/clustering.js --no-entry dv_main.cpp -I "$EIGEN_PATH" -I "$PROJECT_ROOT" -s "EXPORTED_FUNCTIONS=['_malloc', '_free', '_clusterPoints', '_clusterStrings']" -s "EXPORTED_RUNTIME_METHODS=['ccall']" -s ALLOW_MEMORY_GROWTH
+#em++ -o ../../build/clustering.js --no-entry dv_main.cpp -I "$EIGEN_PATH" -I "$PROJECT_ROOT" -s "EXPORTED_FUNCTIONS=['_malloc', '_free', '_clusterPoints', '_clusterStrings']" -s "EXPORTED_RUNTIME_METHODS=['ccall']" -s ALLOW_MEMORY_GROWTH
+em++ -o ../../build/clustering.js --no-entry dv_main.cpp ./external/hclust/fastcluster.cpp ./distmat/distmat.cpp ./scaling/scikit.cpp ./scaling/smacof.cpp -I "$EIGEN_PATH" -I "$PROJECT_ROOT" -s "EXPORTED_FUNCTIONS=['_malloc', '_free', '_clusterPoints', '_clusterStrings']" -s "EXPORTED_RUNTIME_METHODS=['ccall']" -s ALLOW_MEMORY_GROWTH
 
 # Check if compilation successful
 if [ $? -eq 0 ]
