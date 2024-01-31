6c881b81c7b5a723d534d074715c4ad250bb6de3
kramerqk <kramerqk@proton.me>
2024-01-31 03:09:31 +0100

    added import and export functionality

--- src/js/map.js
+++ src/js/map.js
@@ -1,3 +1,5 @@
+var exportFile;
+
 function mapFunctions(
   labelsResult,
   pointsToPlot,
@@ -7,6 +9,25 @@ function mapFunctions(
   flagColumnNames,
   numflags_array,
 ) {
+  // Export function parameters to a JSON file
+  // will be saved as 'result.json'
+  exportFile = function () {
+    var a = window.document.createElement("a");
+    var jsonText = JSON.stringify([
+      labelsResult,
+      pointsToPlot,
+      n,
+      zoomLevels,
+      clusterInfos,
+    ]);
+    a.href = window.URL.createObjectURL(
+      new Blob([jsonText], { type: "application/json" }),
+    );
+    a.download = "result.json";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
   //initialize
   var data = [];
   var y_coord = 0;
